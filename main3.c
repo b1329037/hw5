432da36f68415cb12ccead904b2d2993512a03e7
b1329037 <sophiechang0731@gmail.com>
2024-12-12 14:41:07 +0800

    Update main3.c

--- main3.c
+++ main3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
 int main()
 {
-    for (int num = 1; num <= 81; num++) {
-        int i = (num - 1) / 9 + 1;
-        int j = (num - 1) % 9 + 1; 
-        printf("%d x %d = %d\n", i, j, i * j);
-        if (j == 9) {
-            printf("\n"); 
+     for(int i =1,j=1;i<=9;j++){
+        printf("%d*%d=%d\t",i,j,i*j);
+        if(j==9){
+            i++;
+            j=0;
+            printf("\n");
         }
     }
+    
     return 0;
 }
